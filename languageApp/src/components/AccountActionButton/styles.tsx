a867fcbf3e826f975b0fa7855fddc904e1e37ddd
ElizabethFlores <lizflores.dev@gmail.com>
2023-07-31 23:03:26 -0600

    Add AccountActionButton component

---
+++ languageApp/src/components/AccountActionButton/styles.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  actionButton: {
+    width: 305,
+    height: 58,
+    backgroundColor: '#7C0000',
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignContent: 'center',
+    margin: 36,
+  },
+  title: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontFamily: 'Roboto-Bold',
+    textAlign: 'center',
+  },
+});
+export default styles;
