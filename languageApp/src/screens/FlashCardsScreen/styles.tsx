9bf627a4175ca10f77c7065cf136cb82c4edaf55
ElizabethFlores <lizflores.dev@gmail.com>
2023-08-04 16:41:39 -0600

    Add FlashCardsScreen

---
+++ languageApp/src/screens/FlashCardsScreen/styles.tsx
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    width: 335,
+    height: 82,
+    backgroundColor: '#5FCDD9',
+    borderRadius: 6,
+    marginTop: 20,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  flashCardText: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: 24,
+    color: '#012030',
+    textAlign: 'center',
+  },
+  statesContainer: {
+    marginTop: 134,
+  },
+});
+export default styles;
