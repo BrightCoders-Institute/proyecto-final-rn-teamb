8857163c286d32deba1e684fd551c88acd4683ef
ruruisu <zaddozgl@gmail.com>
2023-08-08 19:10:46 -0600

    ListeningSongsScreen logic for API

--- languageApp/src/screens/ListeningSongsScreen/ListeningSongsScreen.tsx
+++ languageApp/src/screens/ListeningSongsScreen/ListeningSongsScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 //components
 import { HeaderText } from '../../components/HeaderText/HeaderText';
@@ -7,10 +7,40 @@ import { ListeningCard } from '../../components/ListeningCard/ListeningCard';
 interface ListeningSongsScreenProps {
   accessToken: string | null;
 }
+interface Track {
+  name: string;
+  artists: { name: string }[];
+}
+
 export const ListeningSongsScreen: React.FC<ListeningSongsScreenProps> = ({ accessToken }) => {
+  const [randomSong, setRandomSong] = useState<Track | null>(null);
+
+  const getRandomSong = async () => {
+    try {
+      const token = accessToken;
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=year:2020&type=track&limit=1`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await response.json();
+      const randomTrack = data.tracks.items[0];
+      setRandomSong(randomTrack);
+    } catch (error) {
+      console.error('Error fetching random song:', error);
+    }
+  };
+
+  useEffect(() => {
+    getRandomSong();
+  }, [accessToken]);
+  
   return (
     <View>
-      <Text>Access Token: {accessToken}</Text>
       <HeaderText header={'Listening practice'} />
       <DescriptionText
         description={
