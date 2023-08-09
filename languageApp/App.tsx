3959e358c1479e77da8708461524abb84e4a69bf
ruruisu <zaddozgl@gmail.com>
2023-08-08 18:13:03 -0600

    function to get the token

--- languageApp/App.tsx
+++ languageApp/App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+import axios from 'axios';
+import { ListeningSongsScreen } from './src/screens/ListeningSongsScreen/ListeningSongsScreen';
 
-const App = () => {
+const App: React.FC = () => {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    // get the token
+    axios.get("https://accounts.spotify.com/api/token")
+      .then(response => {
+        const token: string = response.data.access_token;
+        setAccessToken(token);
+      })
+      .catch(error => {
+        console.error('Error fetching access token:', error);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Hello, React Native!</Text>
+    <View>
+     <ListeningSongsScreen/> 
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
