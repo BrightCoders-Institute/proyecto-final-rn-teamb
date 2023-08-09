4d01b9c12894564513ee95f5aa6dc201acb379b7
ruruisu <zaddozgl@gmail.com>
2023-08-08 18:51:13 -0600

    changed request to string for debugging

--- languageApp/App.tsx
+++ languageApp/App.tsx
@@ -9,14 +9,21 @@ const App: React.FC = () => {
   useEffect(() => {
     const fetchAccessToken = async () => {
       try {
+        const requestData = new URLSearchParams();
+        requestData.append('grant_type', 'client_credentials');
+        requestData.append('client_id', 'aa882dee2f694376a697c2c105bbae84');
+        requestData.append('client_secret', 'a00142e9d2e94a2190bd2cbd7c3dd475');
+  
         const response = await axios.post(
           'https://accounts.spotify.com/api/token',
-          new URLSearchParams({
-            'grant_type': 'client_credentials',
-            'client_id': 'aa882dee2f694376a697c2c105bbae84',
-            'client_secret': 'a00142e9d2e94a2190bd2cbd7c3dd475'
-          })
+          requestData.toString(), // Convert the URLSearchParams object to a string
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          }
         );
+  
         const token = response.data.access_token;
         setAccessToken(token);
       } catch (error) {
@@ -26,7 +33,7 @@ const App: React.FC = () => {
   
     fetchAccessToken();
   }, []);
-
+  
   return (
     <View>
      <ListeningSongsScreen accessToken={accessToken}/> 
