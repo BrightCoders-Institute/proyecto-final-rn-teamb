2e8b012c5fb744c7d166d44d987cd78e8103a457
ElizabethFlores <lizflores.dev@gmail.com>
2023-08-15 18:26:04 -0600

    Modify props

--- languageApp/src/components/HeaderText/HeaderText.tsx
+++ languageApp/src/components/HeaderText/HeaderText.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
 
-interface HeaderText {
-  header: String;
+interface HeaderTextProps {
+  header?: String;
 }
 
-export const HeaderText: React.FC<HeaderText> = ({header}) => {
+export const HeaderText: React.FC<HeaderTextProps> = ({header}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>{header}</Text>
