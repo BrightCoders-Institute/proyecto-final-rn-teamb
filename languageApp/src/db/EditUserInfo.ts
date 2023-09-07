72cb753aeaf1afef8930e59c5710a5dd01210084
ruruisu <zaddozgl@gmail.com>
2023-09-07 15:01:01 -0600

    fixed email and name

---
+++ languageApp/src/db/EditUserInfo.ts
@@ -0,0 +1,40 @@
+import { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth'; // Import the auth function
+
+// Modify the editUserInfo function to accept the new email and password
+export const editUserInfo = async (
+  { uid, displayName, email }: FirebaseAuthTypes.User,
+  name?: string,
+  newEmail?: string,
+  newPassword?: string, // Add a parameter for the new password
+) => {
+  if (name !== undefined) displayName = name;
+  try {
+    // Create an object to update in Firestore
+    const updateData: { [key: string]: string } = {
+      firstName: displayName,
+    };
+
+    if (newEmail !== undefined) {
+      updateData.email = newEmail; // Include the new email if provided
+    }
+
+    await firestore().collection('Users').doc(uid).update(updateData);
+
+    if (newPassword) {
+      // Use Firebase Authentication method to change the user's password
+      await auth().currentUser?.updatePassword(newPassword);
+    }
+
+    const updatedUser = await firestore().collection('Users').doc(uid).get();
+    if (updatedUser.exists) {
+      Alert.alert('User information updated');
+    }
+
+    return updatedUser.data();
+  } catch (error) {
+    console.log(error);
+  }
+};
--- languageApp/src/screens/EditProfileScreen/EditProfileScreen.tsx
+++ languageApp/src/screens/EditProfileScreen/EditProfileScreen.tsx
@@ -1,54 +1,95 @@
-import React from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import firestore from '@react-native-firebase/firestore';
-import styles from './styles';
-import {HeaderText} from '../../components/HeaderText/HeaderText';
-import {AccountActionButton} from '../../components/AccountActionButton/AccountActionButton';
-import * as Yup from 'yup';
-import {useFormik} from 'formik';
-import {useDispatch, useSelector} from 'react-redux';
-import {RootState} from '../../store/reducers';
-import {setEmail, setName} from '../../store/userSlice';
 
+import styles from './styles';
+import { HeaderText } from '../../components/HeaderText/HeaderText';
+import { AccountActionButton } from '../../components/AccountActionButton/AccountActionButton';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/reducers';
+import { setEmail, setName } from '../../store/userSlice';
+import { updateInfo } from '../../auth/UpdateInfo'; // Import the updateInfo function for updating user info
+
+// Update the UserProfile interface to include uid
 interface UserProfile {
   email: string;
   name: string;
+  password: string;
+  uid: string; // Add uid to the UserProfile interface
 }
 
 const EditProfile: React.FC = () => {
   const dispatch = useDispatch();
-  const {email, name}: UserProfile = useSelector(
-    (state: RootState) => state.data,
+  const { email, name, password, uid }: UserProfile = useSelector(
+    (state: RootState) => state.data
   );
+  
+  console.log('Redux State:', useSelector((state: RootState) => state));
+  
+
+  // Add this console log to display the uid
+  console.log('User UID:', uid);
+
   const initialValues: UserProfile = {
     email: email,
     name: name,
+    password: '', // Initialize the password field
+    uid: uid, // Pass uid
   };
 
-  // TODO: change values in database
+  const [showPassword, setShowPassword] = useState(false); // State for toggling password visibility
+
   const formik = useFormik({
     initialValues: initialValues,
     validationSchema: Yup.object(validationSchema()),
     validateOnChange: true,
-    onSubmit: formValue => {
+    onSubmit: async (formValue) => {
+      // Update the user profile with new values
       dispatch(setName(formValue.name));
       dispatch(setEmail(formValue.email));
+
+      console.log("FORM VALUE",formValue)
+
+      // Use the updateInfo function for updating user profiles
+      try {
+        await updateInfo(formValue); // This will update the user's profile
+        // Handle success or display any appropriate messages
+      } catch (error) {
+        console.error('Error updating user info:', error);
+        // Handle the error, show a message, etc.
+      }
     },
   });
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <View style={styles.container}>
       <HeaderText header="Edit Profile" />
       <View style={styles.pfpContainer}>
         <View>
-          <Icon name="person-circle" size={100} style={{color: 'black'}} />
+          <Icon name="person-circle" size={100} style={{ color: 'black' }} />
         </View>
         <View style={styles.pfpLabels}>
           <Text style={styles.pfpLabel}>Email</Text>
           <Text style={styles.pfpLabelText}>{formik.values.email}</Text>
           <Text style={styles.pfpLabel}>Name</Text>
           <Text style={styles.pfpLabelText}>{formik.values.name}</Text>
+          <Text style={styles.pfpLabel}>UID</Text>
+          <Text style={styles.pfpLabelText}>{formik.values.uid}</Text>
+          {/* Add a field for editing the password */}
+          <Text style={styles.pfpLabel}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={!showPassword}
+            value={formik.values.password}
+            onChangeText={(text) => formik.setFieldValue('password', text)}
+          />
         </View>
       </View>
 
@@ -57,7 +98,7 @@ const EditProfile: React.FC = () => {
         style={styles.input}
         placeholder="E-mail"
         value={formik.values.email}
-        onChangeText={text => formik.setFieldValue('email', text)}
+        onChangeText={(text) => formik.setFieldValue('email', text)}
       />
       {formik.errors.email && (
         <Text style={styles.errorText}>{formik.errors.email}</Text>
@@ -67,11 +108,30 @@ const EditProfile: React.FC = () => {
         style={styles.input}
         placeholder="Name"
         value={formik.values.name}
-        onChangeText={text => formik.setFieldValue('name', text)}
+        onChangeText={(text) => formik.setFieldValue('name', text)}
       />
       {formik.errors.name && (
         <Text style={styles.errorText}>{formik.errors.name}</Text>
       )}
+      <Text style={styles.label}>Password</Text>
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={[styles.input, styles.passwordInput]}
+          placeholder="Password"
+          secureTextEntry={!showPassword}
+          value={formik.values.password}
+          onChangeText={(text) => formik.setFieldValue('password', text)}
+        />
+        <TouchableOpacity
+          style={styles.passwordToggle}
+          onPress={toggleShowPassword}>
+          <Icon
+            name={showPassword ? 'eye-off' : 'eye'}
+            size={20}
+            color="#777"
+          />
+        </TouchableOpacity>
+      </View>
       <TouchableOpacity
         style={styles.loginButton}
         onPress={formik.handleSubmit}>
