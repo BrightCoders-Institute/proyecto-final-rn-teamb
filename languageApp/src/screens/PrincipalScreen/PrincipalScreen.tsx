2e19dddcb7c1d2d05c9170a5b7078400b260b455
ElizabethFlores <lizflores.dev@gmail.com>
2023-09-08 00:24:04 -0600

    Change button to TouchableHighlight

--- languageApp/src/screens/PrincipalScreen/PrincipalScreen.tsx
+++ languageApp/src/screens/PrincipalScreen/PrincipalScreen.tsx
@@ -1,14 +1,14 @@
-import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {View, Text, TouchableOpacity, TouchableHighlight} from 'react-native';
 import styles from './styles';
-import {HeaderText} from '../../components/HeaderText/HeaderText';
 import {NavigationType} from '../../../types/NavigationType';
+import {HeaderText} from '../../components/HeaderText/HeaderText';
 import Logo from '../../components/Icon/Logo';
 
 export const PrincipalScreen = ({navigation}: NavigationType) => {
   return (
     <View style={styles.container}>
-      <Logo />
+      <Logo style={styles.logo} />
       <HeaderText header="Time to improve your english skills!" />
 
       <View style={styles.buttonsContainer}>
@@ -17,11 +17,11 @@ export const PrincipalScreen = ({navigation}: NavigationType) => {
           onPress={() => navigation.navigate('SignUpScreen')}>
           <Text style={styles.signUpTitle}>Sign Up</Text>
         </TouchableOpacity>
-        <TouchableOpacity
+        <TouchableHighlight
           style={styles.loginButton}
           onPress={() => navigation.navigate('Login')}>
           <Text style={styles.loginTitle}>Login</Text>
-        </TouchableOpacity>
+        </TouchableHighlight>
       </View>
     </View>
   );
