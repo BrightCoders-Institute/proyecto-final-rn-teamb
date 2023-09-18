54cd057aafbb652359d1b6c93d996726953e8a84
ElizabethFlores <lizflores.dev@gmail.com>
2023-09-18 17:08:12 -0600

    Add margin bottom

--- languageApp/src/components/CardList/CardList.tsx
+++ languageApp/src/components/CardList/CardList.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {FlatList, Text} from 'react-native';
+import {FlatList, View} from 'react-native';
 //components
 import {ToReadCard} from '../ToReadCard/ToReadCard';
 import {ListeningCard} from '../ListeningCard/ListeningCard';
-import { PodcastCard } from '../PodcastCard/PodcastCard';
+import {PodcastCard} from '../PodcastCard/PodcastCard';
 //interfaces
 import {PodcastEpisode} from '../../interfaces/CardsInterfaces';
 import {Track} from '../../interfaces/CardsInterfaces';
@@ -22,45 +22,45 @@ const ToReadList: React.FC<{data: Story[]} & NavigationProps> = ({
   data,
   navigation,
 }) => (
-  <FlatList
-    data={data}
-    renderItem={({item}) => (
-      <ToReadCard
-        {...item}
-        onPress={() =>
-          navigation.navigate('StoryScreen', {
-            title: item.title,
-            author: item.author,
-            story: item.story,
-          })
-        }
-      />
-    )}
-    keyExtractor={(item, index) => index.toString()}
-  />
+  <View style={{marginBottom: 40}}>
+    <FlatList
+      data={data}
+      renderItem={({item}) => (
+        <ToReadCard
+          {...item}
+          onPress={() =>
+            navigation.navigate('StoryScreen', {
+              title: item.title,
+              author: item.author,
+              story: item.story,
+            })
+          }
+        />
+      )}
+      keyExtractor={(item, index) => index.toString()}
+    />
+  </View>
 );
 
 const PodcastList: React.FC<{data: PodcastEpisode[]}> = ({data}) => (
-
-  <FlatList
-  data={data}
-  renderItem={({item}) => <PodcastCard podcast={item} />}
-  keyExtractor={(item, index) => index.toString()}
-/>
+  <View style={{marginBottom: 40}}>
+    <FlatList
+      data={data}
+      renderItem={({item}) => <PodcastCard podcast={item} />}
+      keyExtractor={(item, index) => index.toString()}
+    />
+  </View>
 );
 
-const MusicList: React.FC<{data: Track[]}> = ({data}) => {
-  console.log('CARD LIST');
-  console.log(data);
-
-  return (
+const MusicList: React.FC<{data: Track[]}> = ({data}) => (
+  <View style={{marginBottom: 40}}>
     <FlatList
       data={data}
       renderItem={({item}) => <ListeningCard song={item} />}
       keyExtractor={(item, index) => index.toString()}
     />
-  );
-};
+  </View>
+);
 
 const CardList: React.FC<CardListProps & NavigationProps> = ({
   musicData,
