9527e9d546c59c151f6c893014a0bb9e44b3b804
ElizabethFlores <lizflores.dev@gmail.com>
2023-09-18 21:28:12 -0600

    Delete unnecessary code

--- languageApp/src/screens/EditProfileScreen/EditProfileScreen.tsx
+++ languageApp/src/screens/EditProfileScreen/EditProfileScreen.tsx
@@ -1,19 +1,17 @@
-import React, { useEffect, useLayoutEffect } from 'react';
-import {View, Text, TextInput, TouchableOpacity, Keyboard, ScrollView} from 'react-native';
+import React from 'react';
+import {View, Text, ScrollView} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from './styles';
 //components
-import { HeaderText } from '../../components/HeaderText/HeaderText';
-import { AccountActionButton } from '../../components/AccountActionButton/AccountActionButton';
+import {AccountActionButton} from '../../components/AccountActionButton/AccountActionButton';
+import {BlueButton} from '../../components/BlueButton/BlueButton';
 //navigation
-import { NavigationProp } from '@react-navigation/native';
+import {NavigationProp} from '@react-navigation/native';
 //redux
-import * as Yup from 'yup';
-import { useFormik } from 'formik';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../store/reducers';
-import { setEmail, setName } from '../../store/userSlice';
-import { updateInfo } from '../../auth/UpdateInfo';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../../store/reducers';
+import {setEmail, setName} from '../../store/userSlice';
+import {useFormik} from 'formik';
 
 interface EditProfileProps {
   navigation: NavigationProp<any>;
@@ -26,8 +24,8 @@ interface UserProfile {
 
 const EditProfile: React.FC<EditProfileProps> = ({navigation}) => {
   const dispatch = useDispatch();
-  const { email, name }: UserProfile = useSelector(
-    (state: RootState) => state.data
+  const {email, name}: UserProfile = useSelector(
+    (state: RootState) => state.data,
   );
 
   const initialValues: UserProfile = {
@@ -37,53 +35,17 @@ const EditProfile: React.FC<EditProfileProps> = ({navigation}) => {
 
   const formik = useFormik({
     initialValues: initialValues,
-    validationSchema: Yup.object(validationSchema()),
-    validateOnChange: true,
-    onSubmit: async (formValue) => {
-
+    onSubmit: async formValue => {
       dispatch(setName(formValue.name));
       dispatch(setEmail(formValue.email));
-
-      console.log("FORM VALUE", formValue)
-
-      try {
-        await updateInfo(formValue);
-
-      } catch (error) {
-        console.error('Error updating user info:', error);
-      }
     },
   });
 
-  useEffect(() => {
-    let parentNav = navigation.getParent();
-    const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
-      parentNav.setOptions({
-        tabBarStyle: {display: 'none'},
-      });
-    });
-    const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
-      parentNav.setOptions({
-        tabBarStyle: {
-          display: 'flex',
-          backgroundColor: '#012030',
-          height: 60,
-        },
-      });
-    });
-
-    return () => {
-      showSubscription.remove();
-      hideSubscription.remove();
-    };
-  }, []);
-
   return (
     <ScrollView style={styles.container}>
-
       <View style={styles.pfpContainer}>
         <View>
-          <Icon name="person-circle" size={100} style={{ color: '#012030' }} />
+          <Icon name="person-circle" size={100} style={{color: '#012030'}} />
         </View>
         <View style={styles.pfpLabels}>
           <Text style={styles.pfpLabel}>Email</Text>
@@ -93,45 +55,13 @@ const EditProfile: React.FC<EditProfileProps> = ({navigation}) => {
         </View>
       </View>
 
-      <Text style={styles.label}>E-mail</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="E-mail"
-        value={formik.values.email}
-        onChangeText={(text) => formik.setFieldValue('email', text)}
+      <BlueButton
+        title="Change Password"
+        onPress={() => navigation.navigate('UpdatePasswordScreen')}
       />
-      {formik.errors.email && (
-        <Text style={styles.errorText}>{formik.errors.email}</Text>
-      )}
-      <Text style={styles.label}>Name</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        value={formik.values.name}
-        onChangeText={(text) => formik.setFieldValue('name', text)}
-      />
-      {formik.errors.name && (
-        <Text style={styles.errorText}>{formik.errors.name}</Text>
-      )}
-
-      <TouchableOpacity style={styles.loginButton} onPress={formik.handleSubmit}>
-        <Text style={styles.buttonText}>Save</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('UpdatePasswordScreen')}>
-        <Text style={styles.buttonText}>Change Password</Text>
-      </TouchableOpacity>
-
       <AccountActionButton title="Log Out" />
-      <AccountActionButton title="Delete Account" />
     </ScrollView>
   );
-};
-
-const validationSchema = () => {
-  return {
-    email: Yup.string().email().required('Email is required'),
-    name: Yup.string().required('The name is required'),
-  };
 };
 
 export default EditProfile;
--- languageApp/src/screens/EditProfileScreen/styles.tsx
+++ languageApp/src/screens/EditProfileScreen/styles.tsx
@@ -1,5 +1,8 @@
 import {StyleSheet} from 'react-native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,11 +10,6 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff',
   },
-  // headerText: {
-  //   fontSize: 24,
-  //   fontWeight: 'bold',
-  // },
-
   pfpContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -36,56 +34,11 @@ const styles = StyleSheet.create({
   },
   pfpLabel: {
     fontSize: 16,
-    fontWeight: 'bold',
+    color: '#012030',
+    fontFamily: 'Roboto-Regular',
   },
   pfpLabelText: {
     fontSize: 16,
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginTop: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  passwordContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  passwordInput: {
-    flex: 1, // Expand the password field all the way
-  },
-  passwordToggle: {
-    position: 'absolute',
-    right: 10,
-    padding: 5,
-  },
-  loginButton: {
-    padding: 12,
-    width: wp("80%"),
-    height: hp("7%"),
-    backgroundColor: '#0D2038',
-    borderRadius: 6,
-    justifyContent: 'center',
-    alignContent: 'center',
-    margin: 15,
-  },
-  errorText: {
-    marginTop: 0,
-    fontSize: 15,
-    color: '#ff0d10',
-    marginLeft: 12,
   },
 });
 
